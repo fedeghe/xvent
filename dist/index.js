5849fe1690e7e0e510634ca1d3cc45f9b9a1c70a
fedeghe <fedeghe@gmail.com>
2019-10-06 20:56:35 +0200

    zero

--- dist/index.js
+++ dist/index.js
@@ -13,4 +13,51 @@ d888" Y888*"   ^Y8888*""  '8888c. .+  "*88*" 8888"  ^%888*
                              "YP'
 1.0.0
 */
-var xvent=function(){function t(t){return JSON.parse(t.replace(/'/g,'"'))}var n=function(t){t=t||JMVC.W.event;var n=t.currentTarget||void 0!==t.target?t.target:t.srcElement;if(!n)return!1;for(;3===n.nodeType&&null!==n.parentNode;)n=n.parentNode;return n},e=function(t){t.stopPropagation&&t.stopPropagation(),null!==t.cancelBubble&&(t.cancelBubble=!0)},i=function(t,n,e){this.node=t,this.attrAct=n||!1,this.attrPar=e||!1,this.map={},this.binded=!1,this.listening=!0};return i.prototype.listen=function(t,n,e,i){return this.map[t]||(this.map[t]=e?{}:[]),n.bubble=!!i,e?(this.map[t][e]||(this.map[t][e]=[]),this.map[t][e].push(n)):this.map[t].push(n),this},i.prototype.clean=function(t,n){this.map[t]&&n?this.map[t][n]&&delete this.map[t][n]:delete this.map[t]},i.prototype.enable=function(){this.listening=!0},i.prototype.disable=function(){this.listening=!1},i.prototype.bind=function(){var i=this;i.binded||(i.map.forEach(),JMVC.each(i.map,function(a,r){JMVC.each(a,function(a,o){JMVC.events.on(i.node,r,function(r){if(!i.listening)return!1;var o=n(r),s=r.currentTarget,p=r.type,u=JMVC.dom.attr(o,i.attrAct),c=JMVC.dom.attr(o,i.attrPar);if(a instanceof Array){if(!(u=!!u.length&&u.split("|")))return!1;c={event:r,node:o,realtarget:s,params:!!c.length&&t(c)},JMVC.each(u,function(t){i.map[p]&&i.map[p][t]&&function(t,n){JMVC.each(t,function(t){t(n,o,s),!t.bubble&&e(r)})}(i.map[p][t],c)})}else a.apply(i.node,[r,o,s]),!a.bubble&&e(r)})})}),this.binded=!0)},{list:[],add:function(t,n,e){var a=new i(t,n,e);return this.list.push(a),a},disable:function(){JMVC.each(this.list,function(t){t.disable()})},enable:function(){JMVC.each(this.list,function(t){t.enable()})}}}();
+
+    var eventTarget = function (e) {
+            e = e || JMVC.W.event;
+            var targetElement = e.currentTarget || (typeof e.target !== 'undefined') ? e.target : e.srcElement;
+            if (!targetElement) {
+                return false;
+            }
+
+            while (targetElement.nodeType === 3 && targetElement.parentNode !== null) {
+                targetElement = targetElement.parentNode;
+            }
+
+            return targetElement;  
+        },
+        stopPropagation = function (e) {
+            if (e.stopPropagation) {
+                e.stopPropagation();
+            }
+            if (e.cancelBubble !== null) {
+                e.cancelBubble = true;
+            }
+        },
+        getParams = function (att) {
+            eval('var t = '+ att)
+            return t;
+            // return JSON.parse(att.replace(/'/g, '"'));
+        },
+        onHash = function (o, fun) {
+            for (var i in o) o.hasOwnProperty(i) && fun(o[i], i);
+        },
+        Area = function (node, attrAct, attrPar) {
+            this.node = node;
+            this.attrAct = attrAct || false;
+            this.attrPar = attrPar || false;
+            this.map = {};
+            this.binded = false;
+            this.listening = true;
+        };
+
+    Area.prototype.listen = function (eventType, func, action, bubble) {
+        return this;
+    };
+
+
+
+    Area.prototype.bind = function () {
+  
+    };
--- source/index.js
+++ source/index.js
@@ -1,23 +1,3 @@
-var xvent = (function () {
-
-    var yyy = {
-        click: {
-            alert: [null, null]
-        },
-        dblclick: {
-            alert2: [null]
-        },
-        mouseover: {
-            overout: [null],
-            blood: [null]
-        },
-        mouseout: {
-            overout: [null]
-        },
-        change: {
-            sel: [null]
-        }
-    }
 
     var eventTarget = function (e) {
             e = e || JMVC.W.event;
@@ -40,6 +20,14 @@ var xvent = (function () {
                 e.cancelBubble = true;
             }
         },
+        getParams = function (att) {
+            eval('var t = '+ att)
+            return t;
+            // return JSON.parse(att.replace(/'/g, '"'));
+        },
+        onHash = function (o, fun) {
+            for (var i in o) o.hasOwnProperty(i) && fun(o[i], i);
+        },
         Area = function (node, attrAct, attrPar) {
             this.node = node;
             this.attrAct = attrAct || false;
@@ -50,96 +38,11 @@ var xvent = (function () {
         };
 
     Area.prototype.listen = function (eventType, func, action, bubble) {
-        this.map[eventType] || (this.map[eventType] = action ? {} : []);
-        // bubbles
-        func['bubble'] = !!bubble;
-        if (action) {
-            this.map[eventType][action] || (this.map[eventType][action] = []);
-            this.map[eventType][action].push(func);
-        } else {
-            this.map[eventType].push(func);
-        }
-        return this;
+
     };
 
-    Area.prototype.clean = function (eventType, action) {
-        this.map[eventType] &&
-            !!action
-            ? (this.map[eventType][action] && (delete this.map[eventType][action]))
-            : (delete this.map[eventType]);
-    };
 
-    Area.prototype.enable = function () { this.listening = true; };
-
-    Area.prototype.disable = function () { this.listening = false; };
 
     Area.prototype.bind = function () {
-        var self = this;
-        // console.debug(self.map)
-        if (!self.binded) {
-            // loop the map
-            self.map.forEach()
-
-            JMVC.each(self.map, function (fnArr, ev) {
-                
-                JMVC.each(fnArr, function (fn, i) {
-                    JMVC.events.on(self.node, ev, function (e) {
-                        if (!self.listening) { return false; }
-
-                        var trg = eventTarget(e),
-                            realtrg = e.currentTarget,
-                            eventType = e.type,
-                            act = JMVC.dom.attr(trg, self.attrAct),
-                            par = JMVC.dom.attr(trg, self.attrPar);
-
-                        // if is array
-                        if (fn instanceof Array) {
-                            act = act.length ? act.split('|') : false;
-                            if (!act) { return false; }
-                            par = { 'event': e, 'node': trg, 'realtarget': realtrg, 'params': par.length ? getParams(par) : false };
-
-                            JMVC.each(act, function (a) {
-                                self.map[eventType] &&
-                                    self.map[eventType][a] &&
-                                    (function (el, p) {
-                                        JMVC.each(el, function (f) {
-                                            f(p, trg, realtrg);
-                                            !f.bubble && stopPropagation(e);
-                                        });
-                                    })(self.map[eventType][a], par);
-                            });
-                        } else {
-                            fn.apply(self.node, [e, trg, realtrg]);
-                            !fn.bubble && stopPropagation(e);
-                        }
-                    });
-                });
-            });
-            this.binded = true;
-        }
+  
     };
-
-    function getParams (att) {
-        return JSON.parse(att.replace(/'/g, '"'));
-    }
-
-    return {
-        list: [],
-        add: function (node, attrAct, attrPar) {
-            var newarea = new Area(node, attrAct, attrPar);
-            this.list.push(newarea);
-            return newarea;
-        },
-        disable: function () {
-            JMVC.each(this.list, function (e) {
-                e.disable();
-            });
-        },
-        enable: function () {
-            JMVC.each(this.list, function (e) {
-                e.enable();
-            });
-        }
-    };
-
-})();
